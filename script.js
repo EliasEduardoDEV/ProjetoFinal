e29caf4dcae730772dd4ebd8c76a98ef210dc1d0
Luana Aguilar <lalvesaguilar@gmail.com>
2024-09-10 16:50:41 -0300

    array

---
+++ script.js
@@ -0,0 +1,252 @@
+const perguntas = [
+    {
+        "O poder da fruta Ope Ope no Mi pertence a qual personagem?":
+            [
+                "A) Donquixote  Doflamingo",
+                "B) Mokey D. Luffy",
+                "C) Trafalgar Law",
+                "D) Eustass Kid"
+            ]
+    },
+
+    {
+        "Qual o nome da ilha que guarda o tesouro one piece?":
+            [
+                "A) Laugh Tale",
+                "B) Ruluka Island",
+                "C) Hyokaido",
+                "D) Green Bit",
+            ]
+    },
+
+    {
+        "Qual o nome do navio do considerado rei dos piratas?":
+            [
+                "A) Moby Dick",
+                "B) Oro Jackson",
+                "C) Going Merry",
+                "D) Red Force"
+            ]
+    },
+
+    {
+        "Qual foi o primeiro shichibukais derrotado por Luffy?":
+            [
+                "A) Crocodile",
+                "B) Gecko Moriah",
+                "C) Donquixote doflamingo",
+                "D) Bartholomew kuma"
+            ]
+    },
+
+    {
+        "Qual foi o yonkous que morreu na guerra de marineford?":
+            [
+                "A) Barba negra",
+                "B) Shanks",
+                "C) Big mom",
+                "D) Barba branca"
+            ]
+    },
+
+    {
+        "O Itadori, é hospedeiro de qual desses?":
+            [
+                "A) Kurama",
+                "B) Ryomen Sukuna",
+                "C) Momoshiki Otsutsuki",
+                "D) Jinchuuriki"
+            ]
+    },
+
+    {
+        "Qual desses é o pai de Megumi Fushiguro ?":
+            [
+                "A) Toji",
+                "B) Satoru Gojo",
+                "C) Yuta Okkotsu",
+                "D) Gabimaru"
+            ]
+    },
+
+    {
+        "Qual o nome da maldição que Yuta Okkotsu carrega ?":
+            [
+                "A) Rika Orimoto",
+                "B) Kyubi",
+                "C) Hanami",
+                "D) Dagon "
+            ]
+    },
+
+    {
+        "Qual é a técnica amaldiçoada de Satoru Gojo ?":
+            [
+                "A) Controle de Fogo",
+                "B) Expansão de Domínio: Infinito Vazio",
+                "C) Manipulação de Sombras",
+                "D) Controle de Raio"
+            ]
+    },
+
+    {
+        "Qual é a técnica de expansão de domínio de Satoru Gojo ?":
+            [
+                "A) Jardim dos Espinhos",
+                "B) Mar de Areia e Sangue",
+                "C) Céu Sem Limites",
+                "D) Infinito Vazio"
+            ]
+    },
+
+    {
+        "Quem é o personagem principal de Demon Slayer?":
+            [
+                "A) Tomioka",
+                "B) Zenitsu",
+                "C) Tanjiro",
+                "D) Nezuko"
+            ]
+    },
+
+    {
+        "Qual é o Hashira mais forte?":
+            [
+                "A) Rengoku",
+                "B) Tomioka",
+                "C) Obanai",
+                "D) Gyomei Himejima"
+            ]
+    },
+
+    {
+        "3 - Qual o nome do mestre dos Hashiras ?":
+            [
+                "A) Muzan",
+                "B) Urokodaki",
+                "C) Genya",
+                "D) Kanroji"
+            ]
+
+    },
+
+    {
+        "Qual é o Lua Superior mais forte ?":
+            [
+                "A) Kokushibou",
+                "B) Doma",
+                "C) Akaza",
+                "D) Kaigaku"
+            ]
+    },
+
+    {
+        "Qual é a técnica de respiração mais forte de Demon Slayer ?":
+            [
+                "A) Respiração da Água",
+                "B) Respiração da Lua",
+                "C) respiração da Serpente",
+                "D) Respiração do Sol"
+            ]
+    },
+
+    {
+        "Qual é o nome da técnica que Goku usou para derrotar o Majin Boo":
+            [
+                "A) Genki Dama",
+                "B) Kamehameha",
+                "C) Kaio-ken ",
+                "D) Super Saiyajin 3"
+            ]
+
+    },
+
+    {
+        "Quantos anos se passaram entre o fim da saga de Majin Boo e o começo do Torneio do Poder?":
+            [
+                "A) 5 anos",
+                "B) 3 anos",
+                "C) 10 anos",
+                "D) 1 ano"
+            ]
+    },
+
+    {
+        "Qual foi o primeiro personagem a se transformar em Super Saiyajin 2? ":
+            [
+                "A) Vegeta",
+                "B) Gohan",
+                "C) Trunks",
+                "D) Goku"
+            ]
+    },
+
+    {
+        "Quem matou o Androide 16 durante a saga dos Androides?":
+            [
+                "A) Cell",
+                "B) Vegeta",
+                "C) Freeza",
+                "D) Gohan"
+            ]
+    },
+    {
+        "Qual é o nome do planeta onde Goku treinou com o Senhor Kaioh?":
+            [
+                "A) Namekusei",
+                "B) Planeta Vegeta",
+                "C) Planeta Kaioh",
+                "D) Terra"
+            ]
+    },
+    {
+        "Qual destes Jinchuuriki não foi capturado pela Akatsuki?":
+            [
+                "A) Gaara",
+                "B) Yagura",
+                "C) Roshi",
+                "D) Han"
+            ]
+    },
+
+    {
+        "Qual jutsu de Naruto Uzumaki foi inspirado em um animal?":
+            [
+                "A) Rasengan",
+                "B)Rasenshuriken",
+                "C)Onda Trovejante",
+                "D)Multi-clones das Sombras"
+            ]
+    },
+
+    {
+        "Qual o nome da técnica que permite ao usuário manipular o chakra para criar formas sólidas, como armas ou escudos? ":
+            [
+                "A) Ninjutsu de Formação",
+                "B) Ninjutsu de Transformação ",
+                "C) Ninjutsu de Invocação",
+                "D) Ninjutsu de Ilusão"
+            ]
+    },
+
+    {
+        "Qual dos seguintes personagens não possui um Kekkei Genkai?":
+            [
+                "A) Rock Lee",
+                "B) Neji Hyuga",
+                "C) Itachi Uchiha",
+                "D) Hidan"
+
+            ]
+    },
+
+    {
+        "Qual o nome da técnica que permite ao usuário transferir sua própria vida para outra pessoa?":
+            [
+                "A) Izanagi",
+                "B) Izanagi",
+                "C) Shiki Fujin",
+                "D) Kuchiyose: No Jutsu"
+            ]
+    }
+]
